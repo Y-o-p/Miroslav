531feadc41feff96afbb5ab3dc34419b76a438e1
Sydney Melvin <sm0296@uah.edu>
2023-11-17 13:31:39 -0600

    Create player left and right movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,65 +5,87 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D playerRigidbody;
-    Animator animator;
-    float jumpForce = 340.0f;
-    float runForce = 10.0f;
-    float maxRunSpeed = 2.0f;
+    float speed = 10.0f;
 
-    private bool _isMoving = false;
-
-    public bool IsMoving {
-        get { 
-            return _isMoving; 
-        }
-        set { 
-            _isMoving = value;
-            animator.SetBool("IsMoving", _isMoving);
-        }
-    }
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Jump
-        if (Input.GetKeyDown(KeyCode.UpArrow) && this.playerRigidbody.velocity.y == 0)
-        {
-            this.playerRigidbody.AddForce(transform.up * this.jumpForce);
-        }
+        playerRigidbody.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, playerRigidbody.velocity.y);
+    } 
+}   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//     Animator animator;
+//     float jumpForce = 340.0f;
+//     float runForce = 10.0f;
+//     float maxRunSpeed = 2.0f;
 
-        // move left and right
-        int key = 0;
-        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
-        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+//     private bool _isMoving = false;
 
-        // decide PC's speed
-        float speedx = Mathf.Abs(this.playerRigidbody.velocity.x);
+//     public bool IsMoving {
+//         get { 
+//             return _isMoving; 
+//         }
+//         set { 
+//             _isMoving = value;
+//             animator.SetBool("IsMoving", _isMoving);
+//         }
+//     }
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         playerRigidbody = GetComponent<Rigidbody2D>();
+//         animator = GetComponent<Animator>();
+//     }
 
-        // limit PC's speed and avoid acceleration.
-        if (speedx < this.maxRunSpeed)
-        {
-            this.playerRigidbody.AddForce(transform.right * key * this.runForce);
-        }
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         // Jump
+//         if (Input.GetKeyDown(KeyCode.UpArrow) && this.playerRigidbody.velocity.y == 0)
+//         {
+//             this.playerRigidbody.AddForce(transform.up * this.jumpForce);
+//         }
 
-        // reverse spring according to the direction
-        if (key != 0)
-        {
-            transform.localScale = new Vector3(key, 1, 1);
-        }
+//         // move left and right
+//         int key = 0;
+//         if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+//         if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
 
-        this.animator.speed = speedx/2.0f;
+//         // decide PC's speed
+//         float speedx = Mathf.Abs(this.playerRigidbody.velocity.x);
 
-        if (this.playerRigidbody.velocity.y == 0) {
-            this.animator.speed = speedx / 2.0f;
-        }
-        else {
-            this.animator.speed = 1.0f;
-        }
-    }
-}
+//         // limit PC's speed and avoid acceleration.
+//         if (speedx < this.maxRunSpeed)
+//         {
+//             this.playerRigidbody.AddForce(transform.right * key * this.runForce);
+//         }
+
+//         // reverse spring according to the direction
+//         if (key != 0)
+//         {
+//             transform.localScale = new Vector3(key, 1, 1);
+//         }
+
+//         this.animator.speed = speedx/2.0f;
+
+//         if (this.playerRigidbody.velocity.y == 0) {
+//             this.animator.speed = speedx / 2.0f;
+//         }
+//         else {
+//             this.animator.speed = 1.0f;
+//         }
+//     }
+// }
