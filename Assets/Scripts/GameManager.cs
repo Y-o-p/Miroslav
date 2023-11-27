7fede91e928a5d6970d1c13d3f6849a830cebb52
Winston <iwp0002@uah.edu>
2023-11-26 23:28:01 -0600

    Music, UI, and end game scenes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,12 +13,6 @@ public class GameManager : MonoBehaviour
     //public GameObject gameOverPanel;
     public static int score = 0;
     public static Vector2 respawnPoint;
-
-    private static AudioSource title_song;
-    private static AudioSource win_song;
-    private static AudioSource lose_song;
-    private static AudioSource battle_song;
-    private static AudioSource boss_song;
     private static AudioSource play_death_sound;
 
     //public Text objective;
@@ -32,9 +26,7 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         Debug.Log("Game Manager started");
-        CheckActiveScene();
         //UpdateHUD();
-        update_song();
         //Hides pannels
         //lose_panel.SetActive(false);
         //win_panel.SetActive(false);
@@ -45,44 +37,16 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (game_over)
+        {
+            SceneManager.LoadScene("Game Over");
+        }
+        if (is_winner)
+        {
+            SceneManager.LoadScene("Win Scene");
+        }
     }
 
-    static void update_song()
-    {
-        title_song.Stop();
-        battle_song.Stop();
-        boss_song.Stop();
-        win_song.Stop();
-        lose_song.Stop();
-
-        if (currentScene.name == "Title"&&!game_over)
-        {
-            Debug.Log(currentScene.name+" Music ");
-            title_song.Play();
-        }else if (currentScene.name == "Game" && !game_over)
-        {
-            Debug.Log(currentScene.name + " Music ");
-            battle_song.Play();
-        }
-        else if (currentScene.name == "Boss" && !game_over)
-        {
-            Debug.Log(currentScene.name + " Music ");
-            boss_song.Play();
-        }
-        else if (game_over && is_winner)
-        {
-            win_song.Play();
-        }
-        else if (game_over && !is_winner)
-        {
-            lose_song.Play();
-        }
-        
-
-
-
-    }
     // Displays current game state to hud
     private void UpdateHUD()
     {
